e4c23549033ff8493a0082ad5a065689a3215ac4
elif <elif.yurtkuran@gmail.com>
2023-06-26 15:55:02 +0300

    add footer,login,register

--- template.js
+++ template.js
@@ -1,1 +1,27 @@
-alert; {"Deneme"} 
+// Dark Modu
+let darkModu=()=>{
+    //alert("Dark Modu");
+    document.body.classList.toggle("dark-mode");
+}
+
+//input search
+$(document).ready(function(){
+const searchApi=["Adana", "Balikesir", "Corum", "Denizli", "Edirne", "Malatya", "Van"]
+$("#tags").autocomplete({
+    source:searchApi
+})
+}); //end
+
+
+// Footer
+let newDate=()=>{
+    const date=new Date().getFullYear();
+// JS DOM
+//document.getElementById("date_id").innerHTML=new Date().getFullYear();
+
+// Jquery DOM
+$("#date_id").html(date);
+}
+newDate()
+
+
