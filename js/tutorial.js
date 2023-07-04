42ed46ec79a15e1930bef916f0e266d0ef173f47
elif <elif.yurtkuran@gmail.com>
2023-07-04 20:08:16 +0300

    Math, undefined, isNaN, Infinity

--- js/tutorial.js
+++ js/tutorial.js
@@ -51,4 +51,64 @@ result2=56;
 var result2;
 
 
+///////////////////////////////////////////////////
 // Operators
+// + - * ** / %
+// ! değil &&=ve ||=veya
+// x++
+// x--
+// ++x
+// --x
+
+// = atama
+// ==eşit (türüne bakma)
+// ===eşit (türüne bak)
+
+var number1=40;
+var number2=5;
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
+
+//cast
+//number
+//string()
+var userData=Number(prompt("Lüten sayı giriniz"))
+console.log(userDatab+45);
+
+//undefined
+var result4;
+console.log(result4);
+
+//NaN: Not a Number
+var result5="asd"/4;
+console.log(result5);
+
+// is NaN
+var result6=4;
+console.log(isNaN(result6)); //false: sayıdır
+
+// Infinity
+console.log(4/0);
+
+
+///////////////////////////////////////////////////
+// Math
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.min(4,1,99,555,-100));
+console.log(Math.max(4,1,99,555,-100));
+
+console.log(Math.sqrt(16));
+console.log(Math.abs(-25));
+console.log(Math.pow(2,5));
+
+console.log(Math.floor(3.9));
+console.log(Math.ceil(3.1));
+console.log(Math.round(4,4));
+console.log(Math.round(4,5));
+
+console.log(Math.sin(45));
+console.log(Math.round(Math.random()*9+1));
