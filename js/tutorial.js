86529cc57497bb7539c696260f4a516fdb7cff85
elif <elif.yurtkuran@gmail.com>
2023-07-04 19:48:47 +0300

    whoisting, variable, console, alert, window

--- js/tutorial.js
+++ js/tutorial.js
@@ -12,3 +12,43 @@ console.error("error");
 alert("pop-up bilgilendirme-1");
 window.alert("pop-up bilgilendirme-2");
 window.document.writeln("ekranda yazı");
+
+//Variable
+var result=4423;
+console.log(typeof result+"=>"+result);
+
+var result=44.23;
+console.log(typeof result+"=>"+result);
+
+var result="JS öğreniyorum";
+console.log("result");
+
+var result=true;
+console.log(typeof result+"=>"+result);
+
+// Naming Convensition
+var deneme="JS Data";
+console.log("deneme");
+
+var deneme44="JS Data";
+console.log("deneme44");
+
+var $deneme44="JS Data";
+console.log($deneme44);
+
+var _$deneme44="JS Data";
+console.log(_$deneme44);
+
+var denemeVerisi="JS Data";
+console.log("denemeVerisi");
+
+//var &~44$deneme44="JS Data";
+//console.log(44$deneme44);
+
+
+// whoisitng
+result2=56;
+var result2;
+
+
+// Operators
