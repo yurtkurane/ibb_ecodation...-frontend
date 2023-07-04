e7841c83f3b294b9f7cfcecba88c49566b8f3d67
elif <elif.yurtkuran@gmail.com>
2023-07-04 19:33:42 +0300

    add tutorial.js

---
+++ js/tutorial.js
@@ -0,0 +1,14 @@
+//single Commet
+/*
+multiple Commet
+*/
+
+console.log("loglama");
+console.log("info");
+console.warn("warn");
+console.error("error");
+
+
+alert("pop-up bilgilendirme-1");
+window.alert("pop-up bilgilendirme-2");
+window.document.writeln("ekranda yazı");
